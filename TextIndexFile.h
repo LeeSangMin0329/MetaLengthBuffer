66e9a8c07399e10f34fef7f9eb3e8cd661c06b39
YOURIEPI <dltkdals329@naver.com>
2017-05-23 11:52:08 +0900

    no message

--- TextIndexFile.h
+++ TextIndexFile.h
@@ -1,3 +1,45 @@
+/*
+This class is used to store data using indexes.
+The buffer is a variable field buffer using metadata, and the index is implemented by the IndexElement class.
+The details of the IndexElement class are as follows :
+
+    class IndexElement {
+	public:
+		streampos addr;
+		string field;
+	}
+
+	- field is the key value of the index to be compared,
+	- addr is the absolute descriptor value of the actual record.
+
+< How to use >
+
+	- Initialize the constructor.
+
+		TextIndexFile(string indexFileName, string recordFileName, bool (*compare)(IndexElement a, IndexElement b))
+
+		indexFileName  : The path where Index File
+		recordFileName : The path where the actual record is stored
+		compare		   : (Function Pointer) to compare key to be stored
+
+		* File must exist in path
+
+	- Method.
+
+		bool Read(recType& record, string key);
+			- Store the read record in the record passed as parameter.
+			  Returns true if read was successful or false.
+
+		void Remove(string key);
+			- Find and remove records with the same key.
+
+		void Append(recType& record, string key);
+			- Store record and key received as parameter.
+
+		void PrintIndex();
+			- Print all existing indexes.
+*/
+
 #ifndef __TEXTINDEXFILE__
 #define __TEXTINDEXFILE__
 
